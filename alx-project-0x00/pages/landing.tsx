750f6e2179a73fbff947bb9f97e0993c99d68458
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-09-20 21:31:50 +0100

    Fix: Button display

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,9 +7,10 @@ const Landing: React.FC = () => {
             <h1 className=" text-xl font-extralight">Landing Page</h1>
             <Card />
             
-            <Button styles="px-2 py-1" sizes="small" shapes="rounded-sm" title="Learn More" />
-            <Button styles="px-4 py-2" sizes="medium" shapes="rounded-md" title="Learn More" />
-            <Button styles="px-6 py-3" sizes="large" shapes="rounded-full" title="Learn More" />
+            <Button styles="px-2 py-1" sizes="small" shapes="rounded-sm" title="Small Button" />
+            <Button styles="px-4 py-2" sizes="medium" shapes="rounded-md" title="Medium Button" />
+            <Button styles="px-6 py-3" sizes="medium" shapes="rounded-lg" title="Large Button" />
+            <Button styles="px-8 py-4" sizes="large" shapes="rounded-full" title="Full Button" />
         </div>
 
     )
